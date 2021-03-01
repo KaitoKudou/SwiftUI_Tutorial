980a1c88573804360a57556b81755b3c26b880de
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-02 01:18:57 +0900

    [Add]ファイル追加

---
+++ Landmarks/Landmarks/Model/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by 工藤海斗 on 2021/03/02.
+//
+
+import Foundation
---
+++ Landmarks/Landmarks/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,20 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by 工藤海斗 on 2021/03/02.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge()
+    }
+}
