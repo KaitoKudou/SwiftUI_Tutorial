af10b9308d4953687a83633ccb246e6d0c3effbf
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-01 18:16:34 +0900

    [Add]ファイル追加

---
+++ Landmarks/Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by 工藤海斗 on 2021/03/01.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
---
+++ Landmarks/Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by 工藤海斗 on 2021/03/01.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
