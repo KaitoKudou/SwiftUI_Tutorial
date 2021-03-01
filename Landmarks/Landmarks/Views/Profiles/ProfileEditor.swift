c7e87227d55a34d5f68ce7b2137f9d176400448e
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-02 03:06:17 +0900

    [Add]ファイル追加

---
+++ Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by 工藤海斗 on 2021/03/02.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor()
+    }
+}
