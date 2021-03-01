9ddd5a862308248f708b6f30ffd7b221ea6c31c7
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-01 17:38:19 +0900

    [Add]ホーム画面追加

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 工藤海斗 on 2021/03/01.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
