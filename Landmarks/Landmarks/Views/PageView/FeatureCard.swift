c87f5b7df5a535d9be7df2a16fd6068d2a0b841f
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-02 12:15:03 +0900

    [Add]ファイル追加

---
+++ Landmarks/Landmarks/Views/PageView/FeatureCard.swift
@@ -0,0 +1,20 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by 工藤海斗 on 2021/03/02.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard()
+    }
+}
---
+++ Landmarks/Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,20 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by 工藤海斗 on 2021/03/02.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView()
+    }
+}
