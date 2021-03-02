6036ffd95a6f6f9f731cba4169e3b515d423c720
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-02 12:19:43 +0900

    [Add]UIPageViewをSwiftUI

--- Landmarks/Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -6,15 +6,21 @@
 //
 
 import SwiftUI
+import UIKit
 
-struct PageViewController: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        
+        return pageViewController
     }
-}
-
-struct PageViewController_Previews: PreviewProvider {
-    static var previews: some View {
-        PageViewController()
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
     }
 }
